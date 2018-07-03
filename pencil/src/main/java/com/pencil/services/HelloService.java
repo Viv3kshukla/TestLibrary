01ec2caf6cadf2cd63f6bcd3f6f4d0405130cc08
Viv3kshukla <vivekshukla@iiitdmj.ac.in>
2018-07-03 13:08:38 +0530

    seventh  commit minor changes

--- pencil/src/main/java/com/pencil/services/HelloService.java
+++ pencil/src/main/java/com/pencil/services/HelloService.java
@@ -1,13 +1,24 @@
 package com.pencil.application.services;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloService {
 	
-	
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public String welcome() {
 		return "Welcome Users";
+	}
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public String anyway() {
+		return "Anyway , I don't care ";
+	}
+	
+	
+	public String publicAccess() {
+		return "Hey , this is public access ";
 	}
 	
 }
